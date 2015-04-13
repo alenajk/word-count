186b6fdf2445d4e435e1fddd4740ca6d008a003e
Hackbright Student <info@hackbrightacademy.com>
2015-04-13 16:34:29 -0700

    Uses collections.Counter

--- wordcount.py
+++ wordcount.py
@@ -1,10 +1,12 @@
 # Counts number of times a word occurs in the text file 
 # Accounts for punctuation and case
 # Can call specified file from command line
+# Uses collections.Counter
 
 import sys
+from collections import Counter
 
-words_and_counts = {}
+words_and_counts = Counter()
 
 text_filename = sys.argv[1]
 text_file = open(text_filename)
@@ -14,10 +16,7 @@ for line in text_file:
     lower_words = [word.lower().rstrip(",.?!;:") for word in words_in_line]
 
     for word in lower_words:
-        if word in words_and_counts:
-            words_and_counts[word] += 1
-        else:
-            words_and_counts[word] = 1
+        words_and_counts[word] += 1
 
 for word, count in words_and_counts.items():
     print word, count
